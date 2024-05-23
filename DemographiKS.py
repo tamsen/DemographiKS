3a7c2bc5f01f91fb3d99c2279e748b7e342cd963
tamsen <tamsen.dunn@gmail.com>
2024-05-23 11:19:32 -0700

    empty poject, starting off demography simulator

---
+++ DemographiKS.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+import sys
+from datetime import datetime
+
+
+
+def run_sim():
+
+    #conf = setup(sys.argv)
+    #if not conf:
+    #    return
+
+    print("hello!!")
+
+
+if __name__ == '__main__':
+    run_sim()
