aa437dcdce45875c654c35eaf9c8f49a9f65f765
tamsen <tamsen.dunn@gmail.com>
2024-05-23 14:36:52 -0700

    adding very simple program flow, to go from SLiM to MSPrime to CODEML

--- DemographiKS.py
+++ DemographiKS.py
@@ -1,13 +1,12 @@
 import os
-import shutil
-import sys
-from datetime import datetime#
-#import subprocess, msprime, pyslim
 import subprocess, pyslim
-
 import msprime
 import numpy as np
 import tskit
+from cairosvg import svg2png
+
+from ks_calculator import sequences_to_codeml_in, run_codeml
+from ks_histogramer import get_Ks_from_file, extract_K_values
 
 
 def run_sim():
@@ -23,31 +22,51 @@ def run_sim():
     my_SLiM_script= os.path.join("SLiM_scripts", "TreeSeqRecording.slim")
     trees_file=os.path.join(slim_folder,"Tex1_TS.trees")
     sequence_file=os.path.join(slim_folder,"ex1_TS_overlaid.trees")
+    out_fasta=os.path.join(slim_folder,"ex1_out.fa")
+    out_png=os.path.join(slim_folder,"ex1_out.png")
+    out_csv=os.path.join(slim_folder,"ex1_out.csv")
     subprocess.check_output(["slim", "-m", "-s", "0", my_SLiM_script])
     # Overlay neutral mutations
     ts = tskit.load(trees_file)
 
     #https://tskit.dev/msprime/docs/stable/mutations.html
-    mts = msprime.sim_mutations(ts, rate=1e-11, random_seed=42, keep=True)
+    mts = msprime.sim_mutations(ts, rate=1e-5, random_seed=42, keep=True)
     mts.dump(sequence_file)
-
+    result = mts.draw_svg()
+    svg2png(bytestring=result,write_to=out_png)
     i=0
-
+    ancestral_seq = ["AAA"] * 100
+    mutated_seq = ancestral_seq.copy()
     samp_ids = mts.samples()
-    print("  ID of diploid individual: ", " ".join([f"{ts.node(s).individual:3}" for s in samp_ids]))
-    print("       ID of (sample) node: ", " ".join([f"{s:3}" for s in samp_ids]))
+    print("  ID of diploid individual: \t\t\t", " ".join([f"{ts.node(s).individual:3}" for s in samp_ids]))
+    print("       ID of (sample) node: \t\t\t", " ".join([f"{s:3}" for s in samp_ids]))
     for var in mts.variants():
         #print(str(i))
         site=var.site
         alleles = np.array(var.alleles)
-        print(f"Site {site.id} (ancestral state '{site.ancestral_state}')", alleles[var.genotypes])
+        print(f"Site {site.position} (ancestral state '{site.ancestral_state}')\t", alleles[var.genotypes])
+        original_codon=ancestral_seq[int(site.position)][0:2]+site.ancestral_state
+        print("original_codon="+original_codon)
+        new_codon=ancestral_seq[int(site.position)][0:2]+ alleles[var.genotypes[0]]
+        print("new_codon="+new_codon)
+        ancestral_seq[int(site.position)] = original_codon
+        mutated_seq[int(site.position)] = new_codon
         i=i+1
-        #if i>5:
-        #    return
-    return
 
-    print("Sequence file written here: " + sequence_file)
+    print(ancestral_seq)
+    print(mutated_seq)
+    sequences={"ancestral":"".join(ancestral_seq),
+                "derived":"".join(mutated_seq)}
 
+    fa_file=sequences_to_codeml_in(sequences, out_fasta)
 
+    # need
+    #conda install -c bioconda paml
+    result=run_codeml(fa_file, slim_folder)
+    paml_out_file=result.ML_dS_file
+    #ks_results= get_Ks_from_file(paml_out_file)
+    results = extract_K_values(out_csv, [paml_out_file])
+    print(results)
+    
 if __name__ == '__main__':
     run_sim()
---
+++ ks_calculator.py
@@ -0,0 +1,85 @@
+import os
+import shutil
+from pathlib import Path
+import process_wrapper
+
+
+
+
+def sequences_to_codeml_in(sequences, fa_out_file):
+
+    if len(sequences)== 0:
+        return []
+
+    sequence_names = sequences.keys()
+    with open(fa_out_file, 'w') as f:
+
+        for seq_name in sequence_names:
+            f.writelines(">" + seq_name + "\n")
+            f.writelines(sequences[seq_name] + "\n")
+
+    return fa_out_file
+
+def write_codeml_control_file(template_ctl_file, sequence_file):
+    lines_to_write = []
+    base_name = os.path.basename(sequence_file).replace(".codonalign", "").replace(".fa", "")
+    new_ctl_file = sequence_file.replace(".fa", ".ctl")
+
+    with open(template_ctl_file, 'r') as f:
+
+        while (True):
+
+            line = f.readline()
+            new_line = line
+
+            if "DUMMY.codonalign.fa" in line:
+                new_line = line.replace("DUMMY.codonalign.fa", sequence_file)
+
+            if "mlcTree_DUMMY.out" in line:
+                new_line = line.replace("DUMMY", base_name)
+
+            lines_to_write.append(new_line)
+
+            if not line:
+                break
+
+    with open(new_ctl_file, 'w') as f:
+
+        for line in lines_to_write:
+            f.writelines(line)
+
+    return new_ctl_file
+
+def run_codeml(fa_out_file,out_folder):
+
+
+    template_codeml_ctl_file = get_codeml_ctl_template()
+
+    control_file = write_codeml_control_file(template_codeml_ctl_file, fa_out_file)
+
+    cmd = ["codeml",os.path.basename(control_file)]
+    print("\t cmd: " + " ".join(cmd))
+    print("\t cwd: " + out_folder)
+    print("\t calculating Ks.. ")
+    process_wrapper.run_and_wait_on_process(cmd, out_folder)
+    print("\t Ks determined...")
+    result= codeml_result(out_folder)
+
+    return result
+
+
+def get_codeml_ctl_template():
+    par_dir = Path(__file__).parent
+    template_codeml_ctl_file = os.path.join(par_dir, "paml_input_templates",
+                                            "codeml_input_example.ctl")
+    return template_codeml_ctl_file
+
+
+class codeml_result():
+    ML_dS_file= ""
+    ML_dN_file= ""
+    #ML_file=""
+    def __init__(self, gene_tree_subfolder):
+        self.ML_dS_file = os.path.join(gene_tree_subfolder, "2ML.dS")
+        self.ML_dN_file = os.path.join(gene_tree_subfolder, "2ML.dN")
+        #self.NG_file = os.path.join(gene_tree_subfolder,"2NG.dS")
