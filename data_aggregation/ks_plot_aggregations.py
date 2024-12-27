187eff57ca6b7585c31196acfb8d25f5b729a28a
tamsen <tamsen.dunn@gmail.com>
2024-12-27 11:36:33 -0800

    made more nice plots...

--- data_aggregation/ks_plot_aggregations.py
+++ data_aggregation/ks_plot_aggregations.py
@@ -20,26 +20,31 @@ class TestKsPlotAgg(unittest.TestCase):
         #               'TE03_m12d20y2024_h14m26s56']
 
         TE5_run_list=[
-            'TE03_m12d20y2024_h14m26s56','TE05_m12d23y2024_h08m52s16','TE07_m12d23y2024_h09m18s26']
+            'TE03_m12d20y2024_h14m26s56','TE05_m12d23y2024_h08m52s16','TE07_m12d23y2024_h09m18s26',
+            'TE08_m12d24y2024_h09m31s26','TE09_m12d26y2024_h09m10s55']
         #'TE06_m12d23y2024_h09m10s28',
         #             'TE07_m12d23y2024_h09m18s26']
 
-        Ne=[1000, 1000, 1000]
-        burnin_times_in_generations=[5e7, 5e7, 5e7]
-        time_since_DIV=[1000, 10000,100000]
-
-        run_list_num="5"
+        Ne=[1000, 1000, 1000, 1000, 1000]
+        burnin_times_in_generations=[5e7, 5e7, 5e7, 5e7, 5e7]
+        time_since_DIV=[1000, 10000,100000,500000,1000000]
+        bin_sizes_Tc = [200,200, 200, 200,200]
+        #bin_sizes_Ks = [0.00005,0.0001,0.0004,0.0002,0.0001]
+        bin_sizes_Ks = [0.0002, 0.0002, 0.0002, 0.0002, 0.0002]
+        run_list_num="_5to9"
         num_runs=len(TE5_run_list)
         png_out = os.path.join(demographiKS_out_path,"ks_hist_by_TE{0}_test.png".format(run_list_num))
 
         fig, ax = plt.subplots(2, num_runs, figsize=(20, 10))
         fig.suptitle("SLiM Tcoal by gene in ancestral species at Tdiv\n" + \
-            "Recombination rate = 8e-9, Ne varies, BI varies")
-        xmax = False#0.001  # max(demographiKS_ks_results)
+                     "Recombination rate = 8e-9, Ne and BI constant")
+        #    "Recombination rate = 8e-9, Ne varies, BI varies")
+        xmax_Ks = 0.025#0.001  # max(demographiKS_ks_results)
+        xmax_Tc = False
         #ymax=100
         ymax=False
-        bin_sizes_Ks = 0.00001  # max_Ks/10.0
-        bin_sizes_Tc = [200,200, 200]
+
+
 
         for i in range(0,num_runs):
             run_name=TE5_run_list[i]
@@ -51,8 +56,8 @@ class TestKsPlotAgg(unittest.TestCase):
             plot_title="burnin time=" + str(burnin_times_in_generations[i]) + " gen,\n"\
                         + "Ne=" + str(Ne[i]) + ", Tdiv=" + str(time_since_DIV[i])
             run_duration_in_m = get_run_time_in_minutes(run_path)
-            plot_ks(ax[0,i], demographiKS_ks_results,
-                    run_duration_in_m, plot_title, bin_sizes_Ks, xmax, ymax)
+            plot_ks(ax[0,i], demographiKS_ks_results,time_since_DIV[i],
+                    run_duration_in_m, plot_title, bin_sizes_Ks[i], xmax_Ks, ymax)
 
             slim_csv_file = os.path.join(run_path, "simulated_ancestral_gene_mrcas.csv")
             loci, slim_mrcas_by_gene = read_data_csv(slim_csv_file)
@@ -62,9 +67,10 @@ class TestKsPlotAgg(unittest.TestCase):
             plot_title = "burnin time=" + str(burnin_times_in_generations[i]) + " gen\n" \
                          + "Ne=" + str(Ne[i])
             plot_mrca(ax[1,i], slim_mrcas_by_gene, [], theory_mrcas_by_gene,
-                      run_duration_in_m, plot_title, bin_sizes_Tc[i], xmax, ymax)
+                      run_duration_in_m, plot_title, bin_sizes_Tc[i], xmax_Tc, ymax)
 
         ax[0,0].set(ylabel="# genes in bin")
+        ax[1,0].set(ylabel="# genes in bin")
         plt.tight_layout()
         plt.savefig(png_out, dpi=550)
         plt.clf()
@@ -73,7 +79,7 @@ class TestKsPlotAgg(unittest.TestCase):
         self.assertEqual(True, True)  # add assertion here
 
 
-def plot_ks(this_ax, slim_ks_by_gene,
+def plot_ks(this_ax, slim_ks_by_gene, t_div,
             run_duration_in_m, title, bin_size, xmax, ymax):
 
     num_genes = len(slim_ks_by_gene)
@@ -83,10 +89,14 @@ def plot_ks(this_ax, slim_ks_by_gene,
     bins = np.arange(0, xmax, bin_size)
 
     if len(slim_ks_by_gene) > 0:
-        this_ax.hist(slim_ks_by_gene, bins=bins, facecolor='b', alpha=1.0,
+        this_ax.hist(slim_ks_by_gene, bins=bins, facecolor='b', alpha=0.25,
                      label='SLiM Ks by gene\n'
                            + "(" + str(num_genes) + " genes in genome)",
                      density=False)
+
+    if t_div:
+        t_div_as_ks= t_div * 0.01 * 10**-6
+        this_ax.axvline(x=t_div_as_ks, color='b', linestyle='--', label="input Tdiv as Ks")
 
     x_axis_label = "Ks \n" + "run time: " + str(round(run_duration_in_m, 2)) + " min"
 
